8fedd2c350ac7d700e06ce453c253bfcb8bec2d6
yadavmanish01 <imyadavmanish01@gmail.com>
2026-04-06 15:41:44 +0530

    network api services remainig and github action done

---
+++ lib/data/exceptions/app_exceptions.dart
@@ -0,0 +1,18 @@
+class AppExceptions implements Exception{
+  final _message;
+  final _prefix;
+  AppExceptions([this._message,this._prefix]);
+
+  @override
+  String toString(){
+    return '$_prefix$_message';
+  }
+}
+
+class InternetException extends AppExceptions{
+  InternetException([String? message]):super(message, 'No Internet');
+}
+
+class RequestTimeoutException extends AppExceptions{
+  RequestTimeoutException([String? message]):super(message, 'Request Timeout');
+}
